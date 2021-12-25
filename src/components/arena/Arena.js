d923d20e17dd83846755784e29bd085c0fb1e608
EOussama <oussama_gtacj@live.fr>
2021-12-25 15:45:42 +0100

    🔗 Binds

--- src/components/arena/Arena.js
+++ src/components/arena/Arena.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { CardEntity } from '../../entities/CardEntity';
 import CoinSelect from '../coin-select/CoinSelect';
 import Card from './../card/Card';
@@ -7,13 +8,27 @@ export default function Arena() {
   const card1 = new CardEntity({ name: 'Yetimon', winner: true, points: 68, lvl: 20 });
   const card2 = new CardEntity({ name: 'Dragomon', winner: false, points: 154, lvl: 20 });
 
+  const [bet, setBet] = useState({ value: 0, coin: 'monsta' });
+
   const onBetValueChange = e => {
-    console.log({ onBetValueChange: e });
+    setBet({ ...bet, value: e });
   }
 
   const onBetCoinChange = e => {
-    console.log({ onBetCoinChange: e });
+    setBet({ ...bet, coin: e });
   }
+
+  const onFight = () => {
+    alert('Fight!!!');
+  }
+
+  const onSkip = () => {
+    alert('Skip!!!');
+  }
+
+  useEffect(() => {
+    console.log({ bet });
+  }, [bet]);
 
   return (
     <main className={classes.arena}>
@@ -36,8 +51,8 @@ export default function Arena() {
         </div>
 
         <div className={classes.controls}>
-          <div className={classes.button}>Fight</div>
-          <a className={classes.link}>Skip</a>
+          <div className={classes.button} onClick={onFight}>Fight</div>
+          <a className={classes.link} onClick={onSkip}>Skip</a>
         </div>
       </div>
 
