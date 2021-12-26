7b995304c5557b840274027ff48a87849ac65549
EOussama <oussama_gtacj@live.fr>
2021-12-26 15:51:03 +0100

    📄 Card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,3 +1,9 @@
+import { FaShieldAlt } from 'react-icons/fa';
+import { CgShapeRhombus } from 'react-icons/cg';
+import { RiSwordLine } from 'react-icons/ri';
+
+import Tilt from 'react-tilt';
+
 import classes from './Card.module.css';
 
 export default function Card(props) {
@@ -6,26 +12,34 @@ export default function Card(props) {
   const classkey = `card--${status}`;
 
   return (
-    <div>
-      <div className={classes['card-points']}>{props.card.points}</div>
+    <div className={classes.cardWrapper}>
+      <div className={classes['card-points']}>Power <span className={classes.highlight}>{props.card.points}</span></div>
 
-      <div className={[classes.card, classes[classkey]].join(' ')}>
-        <div className={classes.card__head}>
-          <div className={classes.chip}>
-            <span>{props.card.name}</span>
-            <span>{props.card.lvl}</span>
+      <Tilt className="Tilt" options={{ max: 25, glareEnable: true }} glareEnable={true} >
+        <div className={[classes.card, classes[classkey]].join(' ')}>
+          <div className={classes.card__head}>
+            <div className={classes.chip}>
+              <span>{props.card.name}</span>
+              <span><FaShieldAlt /> {props.card.lvl}</span>
+            </div>
           </div>
-        </div>
 
-        <div className={classes.card__content}>
-          <img className={classes.logo} src={img} alt="Monster" />
-        </div>
+          <div className={classes.card__content}>
+            <img className={classes.logo} src={img} alt="Monster" />
+          </div>
 
-        <div className={classes.card__foot}>
-          <span>{status}!</span>
+          <div className={classes.meta}>
+            <span className={`${classes.metaChip} ${classes['metaChip--energy']}`}><CgShapeRhombus /> 3</span>
+            <span className={`${classes.metaChip} ${classes['metaChip--battle']}`}><RiSwordLine /> 3</span>
+            <span className={`${classes.metaChip} ${classes['metaChip--number']}`}>15581</span>
+          </div>
+
+          <div className={classes.card__foot}>
+            <span>{status}!</span>
+          </div>
+          <div></div>
         </div>
-        <div></div>
-      </div>
+      </Tilt>
     </div>
   );
 }
