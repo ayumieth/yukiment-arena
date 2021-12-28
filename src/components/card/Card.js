a81fefa22deec6f5a69cea1e711cfde4d6ff1608
EOussama <oussama_gtacj@live.fr>
2021-12-28 22:24:45 +0100

    📐 Readjustments

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -12,14 +12,15 @@ export default function Card(props) {
 
   return (
     <div className={classes.cardWrapper}>
-      <div className={classes['card-points']}>Power <span className={classes.highlight}>{props.card?.power}</span></div>
+      {/* <div className={classes['card-points']}>Power <span className={classes.highlight}>{props.card?.power}</span></div> */}
 
       <Tilt className="Tilt" options={{ max: 25, glareEnable: true }} glareEnable={true} >
         <div className={[classes.card, classes[classkey]].join(' ')}>
           <div className={classes.card__head}>
             <div className={classes.chip}>
-              <span>{props.card?.name}</span>
-              <span><FaShieldAlt /> {props.card?.defense}</span>
+              <span title={`Power: ${props.card?.power}`} className={classes.power}>{props.card?.power}</span>
+              <span title={`Name: ${props.card?.name}`}>{props.card?.name}</span>
+              <span title={`Defense: ${props.card?.defense}`}><FaShieldAlt /> {props.card?.defense}</span>
             </div>
           </div>
 
@@ -28,9 +29,9 @@ export default function Card(props) {
           </div>
 
           <div className={classes.meta}>
-            <span className={`${classes.metaChip} ${classes['metaChip--energy']}`}><CgShapeRhombus /> {props.card?.energy}</span>
-            <span className={`${classes.metaChip} ${classes['metaChip--battle']}`}><RiSwordLine /> {props.card?.offense}</span>
-            <span className={`${classes.metaChip} ${classes['metaChip--number']}`}>{props.card?.number}</span>
+            <span title={`Energy: ${props.card?.energy}`} className={`${classes.metaChip} ${classes['metaChip--energy']}`}><CgShapeRhombus /> {props.card?.energy}</span>
+            <span title={`Offense: ${props.card?.offense}`} className={`${classes.metaChip} ${classes['metaChip--battle']}`}><RiSwordLine /> {props.card?.offense}</span>
+            <span title={`Number: #${props.card?.number}`} className={`${classes.metaChip} ${classes['metaChip--number']}`}>{props.card?.number}</span>
           </div>
 
           <div className={classes.card__foot}>
